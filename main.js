e3f09c2020b666488d09c726995ad39da88cb052
Frank Xavio <frank2006x@gmail.com>
2024-10-22 22:02:07 +0530

    basic sketching

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 const gridsize = 400;
-let row = 16;
-let col = 16;
+let sqSize=16;
 
 const sketch_area=document.getElementById('sketch-area');
+
+
+
 sketch_area.style.width=`${gridsize}px`;
 sketch_area.style.height=`${gridsize}px`;
 
+function changeColor(){
+    this.style.background='black'
+}
+
+
 function createGridCells(){
-    for (let i = 0 ; i < (row*col) ;i++){
+    for (let i = 0 ; i < (sqSize*sqSize) ;i++){
         const gridcell=document.createElement("div")
         gridcell.id='cell'
-        gridcell.style.width=`${(gridsize/col)-2}px`;
-        gridcell.style.height=`${(gridsize/col)-2}px`;
+        gridcell.style.width=`${(gridsize/sqSize)-2}px`;
+        gridcell.style.height=`${(gridsize/sqSize)-2}px`;
         sketch_area.appendChild(gridcell);
+
+
+        gridcell.addEventListener('mouseover',changeColor)
+        
+
     }
 
+}
+
+function removeGridCells(){
+    while (sketch_area.firstChild){
+        sketch_area.removeChild(sketch_area.firstChild)
+    }
 }
 createGridCells()
