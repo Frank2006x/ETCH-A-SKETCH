c0a0c9be2fd3cf37495fdf6f4c98213f283cfc7b
Frank Xavio <frank2006x@gmail.com>
2024-10-23 20:15:14 +0530

    pencil & randomizer added

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
 const gridsize =500;
 let sqSize=16;
+let tool = false;
 
 const sketch_area=document.getElementById('sketch-area');
-
+const changeGrid=document.getElementById('changeGrid')
+const pencil=document.getElementById('pencil')
 
 
 sketch_area.style.width=`${gridsize}px`;
 sketch_area.style.height=`${gridsize}px`;
 
+function randomColor(){
+    if (tool==false){
+        let r =Math.random()*256;
+        let g =Math.random()*256;
+        let b =Math.random()*256;
+        return `rgb(${r},${g},${b})`;
+    }
+    else{
+        return 'black'
+    }
+}
 function changeColor(){
-    this.style.background='black'
+    
+    this.style.background=randomColor()
+    let opacity=parseFloat(this.getAttribute('data-opacity'))
+    if (tool==true){
+        
+        if (opacity==1){
+            opacity==1;
+            
+        }
+        else{
+            opacity+=0.1
+        }
+        this.style.opacity=opacity
+        this.setAttribute('data-opacity', opacity);
+
+
+    }
 }
 
 
@@ -19,6 +48,7 @@ function createGridCells(){
         gridcell.id='cell'
         gridcell.style.width=`${(gridsize/sqSize)-2}px`;
         gridcell.style.height=`${(gridsize/sqSize)-2}px`;
+        gridcell.setAttribute('data-opacity', '0');
         sketch_area.appendChild(gridcell);
 
 
@@ -34,4 +64,24 @@ function removeGridCells(){
         sketch_area.removeChild(sketch_area.firstChild)
     }
 }
+
+changeGrid.addEventListener('click',()=>{
+    
+    sqSize=parseInt(prompt('Enter the size of Grid'))
+    while (sqSize<0 || sqSize>100){
+        sqSize=parseInt(prompt('Enter the size of Grid(>0 & <100'))
+    }
+    removeGridCells()
+    createGridCells()
+
+})
+random.addEventListener('click',()=>{
+    tool=false
+})
+pencil.addEventListener('click',()=>{
+    tool=true
+
+})
+
+
 createGridCells()
